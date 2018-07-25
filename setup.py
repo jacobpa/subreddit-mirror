a8185256eb620bf5baeb5a0f9f34faa29ae12790
Jacob Patterson <patterson.882@osu.edu>
2018-07-25 19:28:33 -0400

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="subreddit-mirror",
-    version="1.0.0",
+    version="1.1.0",
     author="Jacob Patterson",
     author_email="jacob@jacobpa.com",
     entry_points={
-        "console_scripts": ['subreddit-mirror = subredditmirror.mirror:main']
+        "console_scripts": ['subreddit-mirror = subredditmirror.__main__']
     },
     classifiers=['Programming Language :: Python :: 3.6',
                  'Operating System :: OS Independent',
-                 'Natural Language :: English']
+                 'Natural Language :: English'],
     description="Copy posts from one subreddit to another",
     install_requires=['progress==1.4', 'praw==5.4.0'],
-    license="GNU GPLv3"
+    license="GNU GPLv3",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=['subredditmirror'],
